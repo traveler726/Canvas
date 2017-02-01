27fcadfe6a0029b24f26d8ddedd3ee018a91ca34
Jennifer Beck <jjbeck@yahoo-inc.com>
2017-02-01 10:27:02 -0800

    Initial Commit

---
+++ Canvas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Canvas
+//
+//  Created by Jennifer Beck on 2/1/17.
+//  Copyright © 2017 JenniferBeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Canvas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Canvas
+//
+//  Created by Jennifer Beck on 2/1/17.
+//  Copyright © 2017 JenniferBeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
